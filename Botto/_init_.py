ebf8b675a4b6e04a7da563c7b0459c039007e1f9
Anshul Garg <93256357+AuraMoon55@users.noreply.github.com>
2021-11-17 15:33:54 +0530

    Create _init_.py

---
+++ Botto/_init_.py
@@ -0,0 +1,40 @@
+import os
+import sys
+from pyrogram import Client
+from telethon import TelegramClient
+from telegram.ext import Updater
+from telegram import ChatAction, ParseMode
+from Botto.Config import Token, workemr, hash, id
+
+TOKEN = Token
+API_ID = id
+WORKERS = workemr
+API_HASH = hash
+
+
+updater = Updater(TOKEN, workers=WORKERS, use_context=True)
+telethn = TelegramClient('ADC_Req', API_ID, API_HASH)
+pgram = Client('ADC_Pyro', api_id=API_ID, api_hash=API_HASH,bot_token=TOKEN)
+dispatcher = updater.dispatcher
+  
+print("Starting Pyrogram Client")
+pgram.start
+print("Aquiring BOT Client Info")
+
+
+bottie = pgram.get_me()
+
+BOT_ID = bottie.id
+BOT_USERNAME = bottie.username
+BOT_NAME = bottie.first_name
+BOT_MENTION = bottie.mention
+
+
+
+end_credits = """
+INFO GATHERED!
+Client: ADC Pyro
+Copyright: (c) 2021 
+AuraMoon55
+"""
+print(end_credits)
