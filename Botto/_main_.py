d25eaf88a5b343f164ba4c2b40a4d34529fd820c
Anshul Garg <93256357+AuraMoon55@users.noreply.github.com>
2021-11-18 22:58:39 +0530

    Update _main_.py

--- Botto/_main_.py
+++ Botto/_main_.py
@@ -27,6 +27,18 @@ Class Starter:
                        InlineKeyboardButton (text="Feeling Lucky", callback_data="h_lucky")]]),
                       )
 
+Class Abt:
+    def _init_(self, name):
+              self.photo= ""
+              self.text=st.ABT_MSG.format(name)
+              self.reply_markup=InlineKeyboardMarkup(
+                     [[InlineKeyboardButton (text="Request", callback_data="h_req"),
+                       InlineKeyboardButton (text="Query", callback_data="h_resp")],
+                      [InlineKeyboardButton (text="Broken Links", callback_data="h_bl"),
+                       InlineKeyboardButton (text="Feeling Lucky", callback_data="h_lucky")]]),
+                      )
+
+
 @run_async 
 def start(update, context):
     if update.effective_chat.type == "private":
@@ -48,10 +60,34 @@ def h_for_func(update, context):
     )
     if match == "req":
         query.message.edit_caption(caption=st.REQ_MSG, reply_markup= InlineKeyboardMarkup(
-        [[InlineKeyboardButton(text="Go back", callback_data="back_btn_help"),
-          InlineKeyboardButton(text="about", callback_data="about")]]
+        [[InlineKeyboardButton(text="REQUEST MANGA", switch_inline_query_current_chat="<manga> "),
+          InlineKeyboardButton(text="REQUEST ANIME", switch_inline_query_current_chat="<anime> ")],
+         [InlineKeyboardButton(text="Go Back", callback_data="back_btn_help")]]
     ))
     elif match == "resp":
         query.message.edit_caption(caption=st.QMSG, reply_markup=markup)
     elif match == "bl":
         query.message.edit_caption(caption=st.BL_MSG, reply_markup=markup)
+
+@run_async
+def back_btn(update, context):
+    query = update.callback_query
+    query.answer()
+    match = query.data.split("_")
+    if "help" in match:
+        return start(update)
+    stuff = Starter(update.effective_user.first_name)
+    query.message.edit_caption(caption=stuff.text, reply_markup=stuff.reply_markup)
+    elif "about" in match:
+         return about(update)
+    stuff = Abt(update.effective_user.first_name)
+    query.message.edit_caption(caption=stuff.text, reply markup=stuff.reply_markup)
+             
+
+
+    start_handler = CommandHandler("start", start)
+    help_funcs_handler = CallbackQueryHandler(h_for_funcs, pattern=r"h_")
+    back_btn_handler = CallbackQueryHandler(back_btn, pattern=r"back_btn")
+
+
+
