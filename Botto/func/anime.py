705a7f3dd0bc111dc396f03854ff0510a72b3778
Anshul Garg <93256357+AuraMoon55@users.noreply.github.com>
2021-11-18 19:00:00 +0530

    Update anime.py

--- Botto/func/anime.py
+++ Botto/func/anime.py
@@ -8,12 +8,25 @@ from telegram.ext import (
     CallbackQueryHandler,
 )
 
+from typing import List, Union
+from Botto.Config import COMMAND_PREFIXES, BOT_NAME
 from telegram.ext.dispatcher import run_async
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton, ForceReply
-
+from pyrogram import Client
 from Botto import dispatcher
-from Botto.helper import strings as st
+from Botto.helper import string as st
 from Botto.helper import sort_caps
 
 base_url = "https://kitsu.io/api/edge"
 tempdict = {}
+
+def command(commands: Union[str, List[str]]):
+    return filters.command(commands, COMMAND_PREFIXES)
+
+@Client.on_message(command["start",f"start@{BOT_NAME}"])
+    def _start(client, message):
+        client.send_message(
+              message.chat.id,
+              text=st.
+
+
